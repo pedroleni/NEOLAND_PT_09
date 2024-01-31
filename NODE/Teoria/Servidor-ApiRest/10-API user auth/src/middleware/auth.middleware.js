a7092af94a34fc9b187ad490ab46e72f3f9ef160
Laura <laurasanchez0301@gmail.com>
2024-01-31 21:13:22 +0100

    comment

---
+++ NODE/Teoria/Servidor-ApiRest/10-API user auth/src/middleware/auth.middleware.js
@@ -0,0 +1,4 @@
+/** es unb middleware como el de subida de ficheros pero con la diferencia que este lo que hace
+ * es comprobar que la persona que entra a las rutas autenticadas este autorizado para hacerlo
+ * El metodo que utiliza para ello es un Token que lo genera la libreria JSON web token
+ */
